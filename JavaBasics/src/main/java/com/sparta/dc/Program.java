53852012f9017b373b6bf9254c7d93fbd2c60791
Devin Colyer <dcolyer@spartaglobal.com>
2024-06-17 16:35:36 +0100

    Changed timeOfDay to be type LocalTime, now accurate to minutes, unit tests updated accordingly

--- JavaBasics/src/main/java/com/sparta/dc/Program.java
+++ JavaBasics/src/main/java/com/sparta/dc/Program.java
@@ -1,15 +1,24 @@
 package com.sparta.dc;
 
+import java.time.LocalTime;
+
 public class Program {
     public static void main(String[] args) {
-        int timeOfDay = 21;
+        LocalTime timeOfDay = LocalTime.of(21, 0);
+        //int timeOfDay = 21;
         System.out.println(getGreeting(timeOfDay));
     }
 
-    public static String getGreeting(int timeOfDay) {
-        if (timeOfDay >= 5 && timeOfDay <= 12) {
+    public static String getGreeting(LocalTime timeOfDay) {
+        if (
+                timeOfDay.isAfter(LocalTime.of(5, 59))
+                        && timeOfDay.isBefore(LocalTime.of(12, 0))
+        ) {
             return "Good morning!";
-        } else if (timeOfDay >= 12 && timeOfDay <= 18) {
+        } else if (
+                timeOfDay.isAfter(LocalTime.of(11, 59))
+                        && timeOfDay.isBefore(LocalTime.of(18, 0))
+        ) {
             return "Good afternoon!";
         } else {
             return "Good evening!";
--- JavaBasics/src/test/java/com/sparta/dc/ProgramTest.java
+++ JavaBasics/src/test/java/com/sparta/dc/ProgramTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalTime;
+
 public class ProgramTest {
 
     // example test with junit
@@ -22,32 +24,41 @@ public class ProgramTest {
     @Test
     @DisplayName("Given a time is between 5 & 12, Then the greeting should be \"Good morning!\"")
     public void testGreetingReturnsGoodMorning() {
-        for (int i = 5; i < 13; i++) {
-            Assertions.assertEquals("Good morning!", Program.getGreeting(i));
+        for (int h = 6; h < 12; h++) { //hour
+            for (int m = 0; m < 60; m++) { // minute
+                Assertions.assertEquals("Good morning!", Program.getGreeting(LocalTime.of(h, m)));
+            }
         }
     }
 
     @Test
     @DisplayName("Given a time is between 12 & 18, Then the greeting should be \"Good afternoon!\"")
     public void testGreetingReturnsGoodAfternoon() {
-        for (int i = 12; i < 19; i++) Assertions.assertEquals("Good afternoon!", Program.getGreeting(i));
-    }
-
-    @Test
-    @DisplayName("Given a time is between 19 & 4, Then the greeting should be \"Good evening!\"")
-    public void testGreetingReturnsGoodEvening() {
-        for (int i = 0; i < 25; i++) {
-            if (i < 5 || i > 18) {
-                Assertions.assertEquals("Good evening!", Program.getGreeting(i));
+        for (int h = 12; h < 18; h++) {
+            for (int m = 0; m < 60; m++) {
+                Assertions.assertEquals("Good afternoon!", Program.getGreeting(LocalTime.of(h, m)));
             }
         }
     }
 
     @Test
-    @DisplayName("Given a time is between invalid, Then the greeting should be \"ERROR MSG HERE\"")
-    public void testGreetingReturnsInvalid() {
-        Assertions.assertEquals("ERROR MSG HERE", Program.getGreeting(-1));
-        Assertions.assertEquals("ERROR MSG HERE", Program.getGreeting(25));
+    @DisplayName("Given a time is between 19 & 4, Then the greeting should be \"Good evening!\"")
+    public void testGreetingReturnsGoodEvening() {
+        for (int h = 0; h < 24; h++) {
+            if (h < 6 || h > 18) {
+                for (int m = 0; m < 60; m++) {
+                        Assertions.assertEquals("Good evening!", Program.getGreeting(LocalTime.of(h, m)));
+                    }
+                }
 
+        }
     }
+
+//    @Test
+//    @DisplayName("Given a time is between invalid, Then the greeting should be \"ERROR MSG HERE\"")
+//    public void testGreetingReturnsInvalid() {
+//        Assertions.assertEquals("ERROR MSG HERE", Program.getGreeting(-1));
+//        Assertions.assertEquals("ERROR MSG HERE", Program.getGreeting(25));
+//
+//    }
 }
