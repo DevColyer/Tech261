f4f7864db55eac48632ebb46b7ac7c3865dd42a5
Devin Colyer <dcolyer@spartaglobal.com>
2024-06-18 10:05:58 +0100

    Improved unit tests, added constant variables, added exception testing

--- JavaBasics/src/main/java/com/sparta/dc/Program.java
+++ JavaBasics/src/main/java/com/sparta/dc/Program.java
@@ -3,6 +3,11 @@ package com.sparta.dc;
 import java.time.LocalTime;
 
 public class Program {
+    public static final LocalTime MORNING_LOWER = LocalTime.of(5, 59);
+    public static final LocalTime MORNING_UPPER = LocalTime.of(12, 0);
+    public static final LocalTime AFTERNOON_LOWER = LocalTime.of(11, 59);
+    public static final LocalTime AFTERNOON_UPPER = LocalTime.of(18, 0);
+
     public static void main(String[] args) {
         LocalTime timeOfDay = LocalTime.of(21, 0);
         //int timeOfDay = 21;
@@ -10,15 +15,9 @@ public class Program {
     }
 
     public static String getGreeting(LocalTime timeOfDay) {
-        if (
-                timeOfDay.isAfter(LocalTime.of(5, 59))
-                        && timeOfDay.isBefore(LocalTime.of(12, 0))
-        ) {
+        if (timeOfDay.isAfter(MORNING_LOWER) && timeOfDay.isBefore(MORNING_UPPER)) {
             return "Good morning!";
-        } else if (
-                timeOfDay.isAfter(LocalTime.of(11, 59))
-                        && timeOfDay.isBefore(LocalTime.of(18, 0))
-        ) {
+        } else if (timeOfDay.isAfter(AFTERNOON_LOWER) && timeOfDay.isBefore(AFTERNOON_UPPER)) {
             return "Good afternoon!";
         } else {
             return "Good evening!";
--- JavaBasics/src/test/java/com/sparta/dc/ProgramTest.java
+++ JavaBasics/src/test/java/com/sparta/dc/ProgramTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.DateTimeException;
 import java.time.LocalTime;
 
 public class ProgramTest {
@@ -22,7 +23,7 @@ public class ProgramTest {
 //    }
 
     @Test
-    @DisplayName("Given a time is between 5 & 12, Then the greeting should be \"Good morning!\"")
+    @DisplayName("Given a time is between 6:00 & 11:59, Then the greeting should be \"Good morning!\"")
     public void testGreetingReturnsGoodMorning() {
         for (int h = 6; h < 12; h++) { // hour
             for (int m = 0; m < 60; m++) { // minute
@@ -32,7 +33,7 @@ public class ProgramTest {
     }
 
     @Test
-    @DisplayName("Given a time is between 12 & 18, Then the greeting should be \"Good afternoon!\"")
+    @DisplayName("Given a time is between 12:00 & 17:59, Then the greeting should be \"Good afternoon!\"")
     public void testGreetingReturnsGoodAfternoon() {
         for (int h = 12; h < 18; h++) { // hour
             for (int m = 0; m < 60; m++) { // minute
@@ -42,17 +43,27 @@ public class ProgramTest {
     }
 
     @Test
-    @DisplayName("Given a time is between 19 & 4, Then the greeting should be \"Good evening!\"")
+    @DisplayName("Given a time is between 18:00 & 5:59, Then the greeting should be \"Good evening!\"")
     public void testGreetingReturnsGoodEvening() {
-        for (int h = 0; h < 24; h++) { // hour
-            if (h == 6) { // skip previously tested times for speed
-                h = 18;
-            }
-
+        for (int h = 0; h < 6; h++) { // hour
             for (int m = 0; m < 60; m++) { // minute
                 Assertions.assertEquals("Good evening!", Program.getGreeting(LocalTime.of(h, m)));
             }
         }
+        for (int h = 18; h < 24; h++) { // hour
+            for (int m = 0; m < 60; m++) { // minute
+                Assertions.assertEquals("Good evening!", Program.getGreeting(LocalTime.of(h, m)));
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("Given an invalid time, Then the method should throw DateTimeException | LocalTime range: 00:00:00 <= x <= 23:59:59")
+    public void testGreetingThrowsExceptionForInvalidTime() {
+        Assertions.assertThrows(DateTimeException.class, () -> Program.getGreeting(LocalTime.of(-1, 0)));
+        Assertions.assertThrows(DateTimeException.class, () -> Program.getGreeting(LocalTime.of(24, 0)));
+        Assertions.assertThrows(DateTimeException.class, () -> Program.getGreeting(LocalTime.of(23, -1)));
+        Assertions.assertThrows(DateTimeException.class, () -> Program.getGreeting(LocalTime.of(23, 60)));
     }
 
 //    @Test
